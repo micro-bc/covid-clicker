131a14a6fa8c72068f5bd8a95a7e96043b10a98c
Jakob Kordež <jakobkordez1999@gmail.com>
2020-12-18 10:29:57 +0100

    Initial commit

---
+++ desktop/src/cc/micro/clicker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cc.micro.clicker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cc.micro.clicker.ClickerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ClickerGame(), config);
+	}
+}
